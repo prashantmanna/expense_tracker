0443e88ce22ba7da16651db505db6429fffae753
Prashant Manna <Prashant12@$>
2025-08-20 19:05:12 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -2,12 +2,18 @@ import 'package:flutter/material.dart';
 
 import 'expenses_list/Expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 96, 59, 181));
 void main(){
   runApp(
        MaterialApp(
         theme: ThemeData().copyWith(
           useMaterial3: true,
-          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)
+          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252),
+          colorScheme:kColorScheme,
+          appBarTheme: AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.primaryContainer,
+          )
         ),
         debugShowCheckedModeBanner: false,
     home: Expenses(),
