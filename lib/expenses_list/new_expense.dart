1f95119ef27c664df70264fae6f7140689252f33
Prashant Manna <Prashant12@$>
2025-08-20 08:33:05 +0530

    second commit

--- lib/expenses_list/new_expense.dart
+++ lib/expenses_list/new_expense.dart
@@ -11,30 +11,78 @@ class NewExpense extends StatefulWidget {
 
 class _NewExpense extends State<NewExpense> {
 
-  var enteredTitle = TextEditingController();
+  final enteredTitle = TextEditingController();
+  final enteredAmount = TextEditingController();
+
+  @override
+  void dispose() {
+    enteredTitle.dispose();
+    enteredAmount.dispose();
+    super.dispose();
+  }
+
+
+  void datePicker(){
+    final now = DateTime.now();
+    final firstDate = DateTime(now.year - 1,now.month , now.day);
+    final lastDate = DateTime(now.year + 1,now.month , now.day);
+    showDatePicker(context: context, firstDate: firstDate, lastDate: lastDate,initialDate: now);
+  }
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
+        // crossAxisAlignment: CrossAxisAlignment.end,
         children: [
           TextField(
-            maxLength: 16,
-            onChanged: _saveInput,
+            maxLength: 50,
+            controller: enteredTitle,
             keyboardType: TextInputType.text,
             decoration: InputDecoration(label: Text("Title")),
           ),
           Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: [
+              Expanded(
+                child: TextField(
+                  keyboardType: TextInputType.number,
+                  decoration: InputDecoration(label: Text("Amount"),prefixText: "\u20B9"),
+                  controller: enteredAmount,
+                ),
+              ),
+              SizedBox(
+                width: 30,
+              ),
+              Expanded(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  children: [
+                    Text("Selected Date"),
+                    IconButton(onPressed: datePicker, icon: Icon(Icons.calendar_month)),
+                  ],
+                ),
+              )
+            ],
+          ),
+
+          Row(
+            children: [
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: Text("Cancel")),
               ElevatedButton(
                 onPressed: (){
-                  print(enteredTitle);
+                  print(enteredTitle.text);
+                  print(enteredAmount.text);
                 },
                 child: const Text("Save Expense"),
               )
             ],
-          )
+          ),
+
 
         ],
       ),
