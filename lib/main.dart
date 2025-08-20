9413a4a29c51d6a6b0bdfe6b0feee5be61501ff3
Prashant Manna <Prashant12@$>
2025-08-20 18:56:51 +0530

    third commit

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,7 @@ void main(){
        MaterialApp(
         theme: ThemeData().copyWith(
           useMaterial3: true,
-          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 250)
+          scaffoldBackgroundColor: Color.fromARGB(255, 220, 189, 252)
         ),
         debugShowCheckedModeBanner: false,
     home: Expenses(),
